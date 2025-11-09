3c685b9785b7d7c0d4353a9691e1b613f69bceaf
Kinan Principe <kinanprincipe@outlook.com>
2025-11-09 13:58:03 -0400

    Configuracoes de compilacao implementada

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 main: main.c
-	gcc main.c -I ./raylib-5.5_linux_amd64/include/ -I ./raygui/src -L ./raylib-5.5_linux_amd64/lib/ -lraylib -lm
+	gcc main.c -I ./bibliotecas/raylib-5.5_linux_amd64/include/ -I ./bibliotecas/raygui-4.0/src -L ./bibliotecas/raylib-5.5_linux_amd64/lib/ -lraylib -lm
+
+clean:
+	rm -rf ./bibliotecas/*
--- configurar_linux.sh
+++ configurar_linux.sh
@@ -1,7 +1,20 @@
-#!/bin bash
+#!/usr/bin/env bash
 # script para configurar a variavel local de biblioteca, permitindo a compilacao
 
 set -e
 
-export LD_LIBRARY_PATH="`pwd`/raylib-5.5_linux_amd64/lib"
-echo Tudo certo!
+make clean
+wget https://github.com/raysan5/raygui/archive/refs/tags/4.0.zip -P ./bibliotecas 
+wget https://github.com/raysan5/raylib/releases/download/5.5/raylib-5.5_linux_amd64.tar.gz -P ./bibliotecas
+
+unzip ./bibliotecas/4.0.zip -d ./bibliotecas/
+tar -xvf ./bibliotecas/raylib-5.5_linux_amd64.tar.gz -C ./bibliotecas/
+
+echo "-------------------------------------------------------------------------"
+echo ""
+echo "Tudo certo!! Agora digite os seguintes comandos no terminal: "
+echo ""
+echo '1.  export LD_LIBRARY_PATH=\"`pwd`/bibliotecas/raylib-5.5_linux_amd64/lib/"'
+echo "2.  make"
+echo ""
+echo "-------------------------------------------------------------------------"
